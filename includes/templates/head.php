d67f685a3068f6f369b862617fd7de25a1445df9
Alexis Chacon <alexis_chacont@hotmail.com>
2025-03-29 02:29:05 -0600

    build: :arrow_up: Update datapicker for JQuery UI

--- includes/templates/head.php
+++ includes/templates/head.php
@@ -10,7 +10,7 @@ function get_head($title)
         <link rel="stylesheet" href="' . ASSETS_PATH . '/css/indivisa.css" type="text/css">
         <link rel="stylesheet" href="' . ASSETS_PATH . '/css/style.css" type="text/css">
         <link rel="stylesheet" href="' . ASSETS_PATH . '/css/fa_all.css" type="text/css">
-
+        
         <script src="' . ASSETS_PATH . '/js/util.js"></script>
     </head>';
 }
