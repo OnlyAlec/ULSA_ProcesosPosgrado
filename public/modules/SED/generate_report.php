0cfa73b775d3f49816c0b3593491aebd90318c4a
Sofi Becerril Pérez <sofibecerril123@outlook.es>
2025-03-31 17:32:51 -0600

    refactor: :adhesive_bandage: Code to create the directory "reports"

--- public/modules/SED/generate_report.php
+++ public/modules/SED/generate_report.php
@@ -80,11 +80,8 @@ function generateReport($students, $filename)
 {
     $pdf = new Fpdf();
     $reportsDir = __DIR__ . '/reports/';
-    if (!is_dir($reportsDir)) {
-        if (!mkdir($reportsDir, 0755, true)) {
-            throw new RuntimeException('Error creating reports directory.');
-        }
-    }
+    if (!is_dir($reportsDir))
+        mkdir($reportsDir, 0777, true);
 
     $pdf->AddFont('IndivisaSans', '', 'IndivisaDisplaySans-Regular.php');
     $pdf->AddFont('IndivisaSerif', '', 'IndivisaDisplaySerif-RegularItalic.php');
