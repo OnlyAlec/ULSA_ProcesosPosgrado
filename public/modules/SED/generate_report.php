7a7931cf1d4c6613e804e6767403b0580baf7c71
Sofi Becerril Pérez <sofibecerril123@outlook.es>
2025-03-31 18:23:54 -0600

    refactor: :adhesive_bandage: Correction in report

--- public/modules/SED/generate_report.php
+++ public/modules/SED/generate_report.php
@@ -93,13 +93,13 @@ function generateReport($students, $filename)
     if (!empty($studentsByEvaluation['EVALUATED'])) {
         addNewPage($pdf);
         $countEvaluated = 0;
+        $countEvaluated = 0;
         $pdf->SetFont('IndivisaSans', '', 14);
         $pdf->Cell(0, 10, mb_convert_encoding('Alumnos que realizaron la Evaluación Docente', 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
         $pdf->Ln(5);
 
         foreach ($studentsByEvaluation['EVALUATED'] as $programName => $programStudents) {
             addTable($pdf, $programName, $programStudents);
-            $countEvaluated = $countEvaluated + count($programStudents);
             $countEvaluated = $countEvaluated + count($programStudents);
         }
     }
@@ -115,9 +115,12 @@ function generateReport($students, $filename)
         }
     }
 
-    $percentage = round(($countEvaluated / count($students)) * 100, 2);
+    $percentage = round(($countEvaluated / count($students))*100, 2);
     addNewPage($pdf);
     $pdf->SetFont('IndivisaSans', '', 14);
+    $pdf->Cell(0, 10, mb_convert_encoding('Números de Alumnos que realizaron la evaluación: ' . $countEvaluated, 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
+    $pdf->Cell(0, 10, mb_convert_encoding('Números de Alumnos que no han realizado la evaluación: ' . (count($students) - $countEvaluated), 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
+    $pdf->Cell(0, 10, mb_convert_encoding('Total de Alumnos: ' . count($students), 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
     $pdf->Cell(0, 10, mb_convert_encoding('Números de Alumnos que realizaron la evaluación: ' . $countEvaluated, 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
     $pdf->Cell(0, 10, mb_convert_encoding('Números de Alumnos que no han realizado la evaluación: ' . (count($students) - $countEvaluated), 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
     $pdf->Cell(0, 10, mb_convert_encoding('Total de Alumnos: ' . count($students), 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
