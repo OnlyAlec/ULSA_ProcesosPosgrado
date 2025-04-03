4cd30c0d62416ea2d73e43495a54c816bc7b99a1
SofiBecerril <SofiBecerril@users.noreply.github.com>
2025-04-03 00:07:29 +0000

    Fix PHP styling 🎨

--- public/modules/SED/generate_report.php
+++ public/modules/SED/generate_report.php
@@ -115,7 +115,7 @@ function generateReport($students, $filename)
         }
     }
 
-    $percentage = round(($countEvaluated / count($students))*100, 2);
+    $percentage = round(($countEvaluated / count($students)) * 100, 2);
     addNewPage($pdf);
     $pdf->SetFont('IndivisaSans', '', 14);
     $pdf->Cell(0, 10, mb_convert_encoding('Números de Alumnos que realizaron la evaluación: ' . $countEvaluated, 'ISO-8859-1', 'UTF-8'), 0, 1, 'L');
