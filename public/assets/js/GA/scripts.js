5322379e2947893f2ce98e283b1e9e9e390054c9
Omar Díaz <omarrdbz@gmail.com>
2025-04-06 23:14:36 -0600

    wip

--- public/assets/js/GA/scripts.js
+++ public/assets/js/GA/scripts.js
@@ -118,17 +118,6 @@ $(document).ready(function () {
     }
 });
 
-$(function () {
-    $(".custom-file-input").on("change", function (e) {
-        const fileName = $(e.target).prop("files")[0]?.name
-            ? $(e.target).prop("files")[0].name.length > 70
-                ? $(e.target).prop("files")[0].name.substring(0, 68) + "..."
-                : $(e.target).prop("files")[0].name
-            : "Seleccionar archivo...";
-        $(e.target).next().text(fileName);
-    });
-});
-
 function setupBtnsGA(name) {
     if (!name) {
         throw new Error("Missing name - setupBtnsGA");
