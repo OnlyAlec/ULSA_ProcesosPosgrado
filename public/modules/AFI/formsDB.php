369085f00492809cee2876ab772522bb6787b4fe
SofiBecerril <SofiBecerril@users.noreply.github.com>
2025-04-07 03:55:05 +0000

    Fix PHP styling 🎨

--- public/modules/AFI/formsDB.php
+++ public/modules/AFI/formsDB.php
@@ -10,7 +10,6 @@ use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
 use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
 use PhpOffice\PhpSpreadsheet\Chart\Title;
 
-
 function init_process($filePath)
 {
     ErrorList::clear();
@@ -312,7 +311,7 @@ function createExcel($students, $programCount)
     $timestamp = date('Y-m-d_H-i-s');
     $outputFile = XLSX_DIR . "/filtered_students_{$timestamp}.xlsx";
     $writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($newSpreadsheet);
-    $writer->setIncludeCharts(true);
+    $writer->setIncludeCharts(true); 
     $writer->save($outputFile);
 
     return $outputFile;
