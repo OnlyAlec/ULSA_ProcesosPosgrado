6eb2b31badf538e8a7d005e0bd249f3f86d23a34
Alexis Chacon <alexis_chacont@hotmail.com>
2025-04-06 20:08:41 -0600

    perf: :goal_net: Standarize catching errors other modules

--- public/modules/SED/index.php
+++ public/modules/SED/index.php
@@ -4,7 +4,6 @@ require_once INCLUDES_DIR . "/utilities/database.php";
 require_once INCLUDES_DIR . "/models/student.php";
 
 ob_start();
-$studentsDB = getStudents();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
     require_once './update_functions.php';
@@ -47,8 +46,8 @@ get_header("Seguimiento de Evaluación Docente");
 ?>
     <main class="container content marco">
         <div class="d-flex justify-content-between align-items-center">
-                <h1 class="mb-2">Lista de alumnos</h1>
-                <a href="../../index.php" class="btn btn-outline-primary">Regresar</a>
+            <h1 class="mb-2">Lista de alumnos</h1>
+            <a href="../../index.php" class="btn btn-outline-primary">Regresar</a>
         </div>
         <br>
         <div class="row justify-content-end">
@@ -87,30 +86,36 @@ get_header("Seguimiento de Evaluación Docente");
             </thead>
             <tbody id="studentsTable">
                 <?php
-            $studentsDB = getStudents();
-foreach ($studentsDB as $student): ?>
-                    <tr data-carrer="<?= $student->getCarrer() ?>">
-                        <td><input type="checkbox" class="studentCheckbox" style="width: 20px; height: 20px;"></td>
-                        <td><?= htmlspecialchars($student->getUlsaId()) ?></td>
-                        <td><?= htmlspecialchars($student->getName()) . " " . htmlspecialchars($student->getLastName()) ?></td>
-                        <td><?= htmlspecialchars($student->getEmail()) ?></td>
-                        <td>
-                            <button class="changeSED border-0" data-student-id="<?= $student->getUlsaId() ?>">
-                                <?= $student->getSed() ? '<i class="fas fa-check-square fa-2x" style="color:#36b18c"></i>' : '<i class="fas fa-minus-square fa-2x" style="color:rgb(206, 85, 85)"></i>' ?>
-                            </button>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
+            if (empty($studentsDB = getStudents())) {
+                echo '<tr><td colspan="5">No hay alumnos registrados.</td></tr>';
+            } else {
+                foreach ($studentsDB as $student): ?>
+                        <tr data-carrer="<?= $student->getProgram() ?>">
+                            <td><input type="checkbox" class="studentCheckbox" style="width: 20px; height: 20px;"></td>
+                            <td><?= htmlspecialchars($student->getUlsaId()) ?></td>
+                            <td><?= htmlspecialchars($student->getName()) . " " . htmlspecialchars($student->getLastName()) ?>
+                            </td>
+                            <td><?= htmlspecialchars($student->getEmail()) ?></td>
+                            <td>
+                                <button class="changeSED border-0" data-student-id="<?= $student->getUlsaId() ?>">
+                                    <?= $student->getSed() ? '<i class="fas fa-check-square fa-2x" style="color:#36b18c"></i>' : '<i class="fas fa-minus-square fa-2x" style="color:rgb(206, 85, 85)"></i>' ?>
+                                </button>
+                            </td>
+                        </tr>
+                <?php endforeach;
+            } ?>
             </tbody>
         </table>
 
         <div class="d-flex justify-content-between">
             <button id="confirmChanges" class="btn btn-success w-50" disabled>Confirmar Cambios</button>
-            <button id="generateReport" class="btn btn-primary" data-filename="reporte_evaluaciones">Generar Reporte</button>
+            <button id="generateReport" class="btn btn-primary" data-filename="reporte_evaluaciones">Generar
+                Reporte</button>
         </div>
 
         <div id="selectedCountContainer">
-            <p style="margin-top:15px; font-size: 20px; font-weight: bold;">Alumnos seleccionados: <span id="selectedCount">0</span></p>
+            <p style="margin-top:15px; font-size: 20px; font-weight: bold;">Alumnos seleccionados: <span
+                    id="selectedCount">0</span></p>
         </div>
     </main>
 
