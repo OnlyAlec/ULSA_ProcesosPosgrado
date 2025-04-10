3b8d18e84ec352924aff3140512df12de14c7e25
Omar Díaz <omarrdbz@gmail.com>
2025-04-09 18:39:16 -0600

    fix: SED update_from_excel

--- public/modules/SED/update_from_excel.php
+++ public/modules/SED/update_from_excel.php
@@ -12,8 +12,8 @@ function processExcel($filePath, $ulsaIdColumn, $nameColumn, $sedColumn)
     ErrorList::clear();
 
     $ulsaIdColumn = strtoupper($ulsaIdColumn);
-    $nameColumn   = strtoupper($nameColumn);
-    $sedColumn    = strtoupper($sedColumn);
+    $nameColumn = strtoupper($nameColumn);
+    $sedColumn = strtoupper($sedColumn);
 
     try {
         $excelStudents = loadExcelStudents($filePath, $ulsaIdColumn, $nameColumn, $sedColumn);
@@ -79,7 +79,7 @@ function loadExcelStudents($filePath, $ulsaIdColumn, $nameColumn, $sedColumn)
             $sedBool = !in_array(strtolower(trim($cellSed)), $falseValues, true);
 
             try {
-                $student = new Student($firstName, $maternalSurname." ".$paternalSurname, $cellUlsaId, carrer: " ", email:"");
+                $student = new Student($firstName, $maternalSurname . " " . $paternalSurname, $cellUlsaId, carrer: " ", email: "");
                 $student->setSed($sedBool);
                 $students[] = $student;
             } catch (InvalidArgumentException $e) {
