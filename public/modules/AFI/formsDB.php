7dff93dc29ffbc3f9e35b76d68cba9a9a136b155
SofiBecerril <SofiBecerril@users.noreply.github.com>
2025-04-29 17:23:12 +0000

    Fix PHP styling 🎨

--- public/modules/AFI/formsDB.php
+++ public/modules/AFI/formsDB.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../includes/config/constants.php';
 require_once VENDOR_DIR . "/autoload.php";
 require_once INCLUDES_DIR . "/utilities/util.php";
