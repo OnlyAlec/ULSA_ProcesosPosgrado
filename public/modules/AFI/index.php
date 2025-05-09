0cb9128b9bd35058f057f547c9f276f4f87aba26
OnlyAlec <OnlyAlec@users.noreply.github.com>
2025-05-09 16:08:21 +0000

    Fix PHP styling 🎨

--- public/modules/AFI/index.php
+++ public/modules/AFI/index.php
@@ -16,7 +16,7 @@ try {
 
             switch ($_POST['action']) {
                 case 'getTableStudents':
-                    $res = array_values(array_map(fn($student) => $student->getJSON(), getStudents()));
+                    $res = array_values(array_map(fn ($student) => $student->getJSON(), getStudents()));
                     break;
                 case 'getMissing':
                     //? Not in use
@@ -112,8 +112,8 @@ get_head("AFI");
 
 <body style="display: block;">
     <?php require_once INCLUDES_DIR . '/templates/header.php';
-    get_header("Avisos de Fechas Importantes");
-    ?>
+get_header("Avisos de Fechas Importantes");
+?>
 
     <main class="container content marco">
         <!-- Botones Nav -->
