6b19cb5a8d13b46f3b0d902f967ee9cded0cd792
onlyalec-pc <alexis@portcities.net>
2025-05-11 22:25:03 -0600

    style: 💄 Update Prettier configuration and add PHP CS Fixer scripts

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,5 +8,6 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'single_quote' => true,
     ])
     ->setFinder($finder);
