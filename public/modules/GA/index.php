7f12caa4c551ce825b7c99f1a1a6528891de8ed5
omarrdbz <omarrdbz@users.noreply.github.com>
2025-05-23 06:10:09 +0000

    Fix Prettier styling 🎨

--- public/modules/GA/index.php
+++ public/modules/GA/index.php
@@ -77,7 +77,7 @@ try {
                 $_POST['email'],
             );
         } elseif ($_POST['action'] === 'getTableStudents') {
-            $res = array_values(array_map(fn ($student) => $student->getJSON(), getStudents()));
+            $res = array_values(array_map(fn($student) => $student->getJSON(), getStudents()));
         } elseif ($_POST['action'] === 'deleteOneStudent') {
             if (!preg_match('/^\d{6}$/', $_POST['claveUlsaDelete'])) {
                 throw new RuntimeException('Clave ULSA invalida. Debe ser un numero de 6 digitos.');
@@ -108,8 +108,8 @@ get_head("GA");
 <body style="display: block;">
     <?php
     require_once INCLUDES_DIR . '/templates/header.php';
-get_header('Gestión de Alumnos');
-?>
+    get_header('Gestión de Alumnos');
+    ?>
 
     <main class="container content marco">
         <!-- Botones Nav -->
