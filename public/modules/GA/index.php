82ece83365c6931b85899e9e2214041a310e6041
omarrdbz <omarrdbz@users.noreply.github.com>
2025-05-26 23:37:37 +0000

    Fix PHP styling 🎨

--- public/modules/GA/index.php
+++ public/modules/GA/index.php
@@ -60,7 +60,7 @@ try {
             );
         } elseif ($_POST['action'] === 'getPrograms') {
             $programs = getPrograms();
-            $res = array_map(fn($program) => $program->getName(), $programs);
+            $res = array_map(fn ($program) => $program->getName(), $programs);
         } elseif ($_POST['action'] === 'registerOneStudent') {
             if (!preg_match('/^\d{6}$/', $_POST['claveUlsa'])) {
                 throw new RuntimeException('Clave ULSA invalida. Debe ser un numero de 6 digitos.');
@@ -87,7 +87,7 @@ try {
                 $_POST['email'],
             );
         } elseif ($_POST['action'] === 'getTableStudents') {
-            $res = array_values(array_map(fn($student) => $student->getJSON(), getStudents()));
+            $res = array_values(array_map(fn ($student) => $student->getJSON(), getStudents()));
         } elseif ($_POST['action'] === 'deleteOneStudent') {
             if (!preg_match('/^\d{6}$/', $_POST['claveUlsaDelete'])) {
                 throw new RuntimeException('Clave ULSA invalida. Debe ser un numero de 6 digitos.');
@@ -116,8 +116,8 @@ get_head('GA');
 <body style="display: block;">
     <?php
     require_once INCLUDES_DIR . '/templates/header.php';
-    get_header('Gestión de Alumnos');
-    ?>
+get_header('Gestión de Alumnos');
+?>
 
     <main class="container content marco">
         <!-- Botones Nav -->
