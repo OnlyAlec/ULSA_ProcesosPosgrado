dd07780b4306803a9f536fd8106f22c990e278b3
EliBece <EliBece@users.noreply.github.com>
2025-06-03 00:14:18 +0000

    Fix Prettier styling 🎨

--- public/modules/AFI/index.php
+++ public/modules/AFI/index.php
@@ -19,7 +19,7 @@ try {
             switch ($_POST['action']) {
                 case 'getTableStudents':
                     $res = array_values(
-                        array_map(fn ($student) => $student->getJSON(), getStudents()),
+                        array_map(fn($student) => $student->getJSON(), getStudents()),
                     );
                     break;
                 case 'getMissing':
@@ -128,9 +128,9 @@ try {
     exit();
 }
 
-$masterProgramsDataForPage = array_map(fn ($program) => $program->getName(), getMastersPrograms());
+$masterProgramsDataForPage = array_map(fn($program) => $program->getName(), getMastersPrograms());
 $specialtyProgramsDataForPage = array_map(
-    fn ($program) => $program->getName(),
+    fn($program) => $program->getName(),
     getSpecialtyPrograms(),
 );
 
@@ -146,8 +146,8 @@ get_head('AFI');
 <body style="display: block;">
     <?php
     require_once INCLUDES_DIR . '/templates/header.php';
-get_header('Avisos de Fechas Importantes');
-?>
+    get_header('Avisos de Fechas Importantes');
+    ?>
 
     <main class="container content marco">
         <!-- Botones Nav -->
